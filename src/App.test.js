ab1fbe13c75cd473d71688d6c84720bb491c06c2
Ting Lew <tlew@OSX-MaheshR.local>
2018-03-06 13:05:14 -0500

    add test boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+
+import reducers from './reducers';
+const store = createStore(reducers)
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
